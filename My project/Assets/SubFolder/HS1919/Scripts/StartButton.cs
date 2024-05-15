5a95422a4de0c0245242d83ebbcb1e7c6a7535ef
HS1919 <sboduoye49@gmail.com>
2024-05-15 09:32:19 +0900

    オセロのゲーム

---
+++ My project/Assets/SubFolder/HS1919/Scripts/StartButton.cs
@@ -0,0 +1,15 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleButton : MonoBehaviour
+{
+    public void Onclick()
+    {
+        //ここで移りたいシーンを指定します。
+        SceneManager.LoadScene("lobby");
+    }
+}
